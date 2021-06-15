3c57a7728b6a0b3e1818ce2b4f94cb81c5bfe638
Vallem Balu <vbnreddy5@gmail.com>
2021-06-15 14:10:32 +0530

    add counter function

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -x
-flip=$((RANDOM%2))
-if [ $flip -eq 1 ]
-then
-    echo  "Heads"
-else
-      echo "Tails"
-fi
+echo " filp coin simulator"
+isTie=0
+hcount=0
+tcount=0
+diff=99
+res=""
+function headTailGenerate(){
+	flip=$((RANDOM%2))
+	if [ $flip -eq 1 ]
+	then
+    		hcount=$(($hcount+1))
+	else
+      		tcount=$(($tcount+1))
+	fi
+}
